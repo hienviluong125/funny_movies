0378e7fb3fd1017f6314b027fe9c36400205c680
hienviluong125 <hienviluong125@gmail.com>
2021-04-08 21:20:50 +0700

    init project structure

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
+
+  validates :password, presence: true, length: { minimum: 6 }, confirmation: false
+end
